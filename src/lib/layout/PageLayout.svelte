99407a7256715ab83f295d83d3f47a6f940e5178
Cooper <Cooperransom08@gmail.com>
2024-12-11 20:03:12 +0000

    update the look of the website again (its so sleek)

--- src/lib/layout/PageLayout.svelte
+++ src/lib/layout/PageLayout.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="flex items-center justify-center p-4 min-h-screen">
-  <div class="mx-auto flex flex-col gap-2 max-w-xs border-mono-divider border h-full p-4">
+  <div class="mx-auto flex flex-col gap-2 w-[20rem] h-full border-mono-divider border p-4">
     {@render children?.()}
   </div>
 </div>
